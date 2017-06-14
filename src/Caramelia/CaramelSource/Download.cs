48ca5d7664e5b717a25cb2c39b7b05cfc8636023
houzkin <houzkin@gmail.com>
2017-06-14 13:01:17 +0900

    a

---
+++ src/Caramelia/CaramelSource/Download.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caramelia.Data.Web {
+    public class DownloadOrder {
+        public bool IsDownloadStockPrice { get; set; }
+        public bool IsDownloadXbrl { get; set; }
+
+    }
+    public class DownloadManager {
+        string _local;
+        
+        public string LocalPath {
+            get { return _local; }
+            set { _local = value; }
+        }
+        public void Download(DownloadManager dm) {
+
+        }
+        public StockPriceDLM StockPriceDlm { get; }
+        public XbrlDLM XbrlDlm { get; }
+
+    }
+    public class StockPriceDLM { }
+    public class XbrlDLM { }
+    public class StockSplitDLM { }
+}
